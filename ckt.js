0c908203ff0db3c9cfcd2107053f3a65b32a47c5
AmaniAlnobani <>
2021-06-16 12:55:43 +0000

    link html with Js

--- ckt.js
+++ ckt.js
@@ -1,5 +1,16 @@
+
 var topic = prompt("what are you lokking for?")
 
-if (topic == "Introduction"){
-  document.write("<p>"+)
+if (topic == "Introduction")
+{
+  document.write("<p>"+"Read carefully"+"</p>")
+}
+else if(topic == "Simple circuit")
+{
+ document.write("<div>"+"<h2>"+"Simple circuit"+"</h2>"+
+        "<img src='https://www.dummies.com/wp-content/uploads/308335.image0.jpg'>"+"</div>")
+}
+else
+ {
+  alert("sorry pick some thing else")
 }
