aa89f073f8d754895342a5a51191faddfdbc1265
AmaniAlnobani <>
2021-06-17 11:43:50 +0000

    add code

--- ckt.js
+++ ckt.js
@@ -1,5 +1,28 @@
-var topic = prompt("what are you lokking for?")
 
-if (topic == "Introduction"){
-  document.write("<p>"+)
+var name = prompt("what is your name?")
+
+alert("welcome to the site")
+
+var topic = prompt("what are you looking for?")
+
+
+while(topic!= "Introduction" && topic!= "Simple circuit"){
+  topic = prompt("try again!")
 }
+if (topic == "Introduction")
+{
+  document.write("<p>"+"Read carefully"+"</p>")
+}
+else if(topic == "Simple circuit"){
+  var numOread = prompt("how many times you want to see it?")
+  for(var x=0; x < numOread; x++){
+
+ document.write("<div>"+"<h2>"+"Simple circuit"+"</h2>"+
+        "<img src='https://www.dummies.com/wp-content/uploads/308335.image0.jpg'>"+"</div>")
+  }
+}
+else
+ {
+  alert("sorry pick some thing else")
+}
+
